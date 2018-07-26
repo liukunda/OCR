4043c1e41a880b44af7502fb77ce86ddb644d38d
liukunda <liukunda@gmail.com>
2018-07-26 13:47:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 from aip import AipOcr
 import json  
 
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
 """ 你的 APPID AK SK """
 APP_ID = '11581347'
 API_KEY = 'lA59Kw0LE0uuNLwaSkmbGr2M'
@@ -15,17 +21,22 @@ def get_file_content(filePath):
     with open(filePath, 'rb') as fp:
         return fp.read()
 
-image = get_file_content('image00003.jpg')
-
 # 定义参数变量  
+
 options = { 
 'detect_direction': 'true',  
 'language_type': 'CHN_ENG',  
 }  
 
+image = get_file_content('image00003.jpg')
+
+
 """ 调用通用文字识别, 图片参数为本地图片 """
 p = client.basicGeneral(image)
-"""格式化 json p 的内容"""
 js = json.dumps(p,sort_keys=True, indent=4, separators=(',', ':')).decode("unicode-escape")
 #print(json.dumps(p).decode("unicode-escape")) 
-print(js)
+#print(js)
+ff = open("1.json","w")
+ff.write(js)
+ff.close()
+print(type(p))
